b1ebda83d9efdd6063cb169e78d3b7cf303e8e4f
Fullops <66186077+Fullops@users.noreply.github.com>
2025-01-01 20:56:12 -0500

    Creación del proyecto, organización de carpeta y creación de componentes principales

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
+import Sidebar from "./components/Sidebar"
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Hola</h1>
+    </>
+  )
+}
+
+export default App
