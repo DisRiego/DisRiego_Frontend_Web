d4f1fb3f9236b48b3243043842fc26d3f9cbddcf
Deivy Santiago Ramon Mora <startlord17@gmail.com>
2025-03-03 18:45:13 -0500

    Add: se añadio codigo a los componentes de confirmation_window y Menssage

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,7 +1,45 @@
 const Message = () => {
   return (
     <>
-      <h1>Mensaje</h1>
+      <div class="notification-container" id="successNotification">
+        <div class="notification is-success is-light p-4">
+          <button
+            class="delete"
+            onclick="hideNotification('successNotification')"
+          ></button>
+          <div class="media">
+            <div class="media-left">
+              <span class="icon has-text-success">
+                <i class="fas fa-check-circle"></i>
+              </span>
+            </div>
+            <div class="media-content">
+              <p class="has-text-weight-bold">Creación de rol con éxito</p>
+              <p>Se ha creado el rol correctamente</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="notification-container" id="errorNotification">
+        <div class="notification is-danger is-light p-4">
+          <button
+            class="delete"
+            onclick="hideNotification('errorNotification')"
+          ></button>
+          <div class="media">
+            <div class="media-left">
+              <span class="icon has-text-danger">
+                <i class="fas fa-times-circle"></i>
+              </span>
+            </div>
+            <div class="media-content">
+              <p class="has-text-weight-bold">Creación de rol fallida</p>
+              <p>No se pudo crear el rol, intente de nuevo</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
